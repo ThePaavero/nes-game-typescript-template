5d9d230ad5f23ebcc085d0db6e99dc3498809010
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2021-12-01 18:53:45 +0200

    Preloading messing around...

--- src/engine/ImagePreloader.ts
+++ src/engine/ImagePreloader.ts
@@ -1,21 +1,31 @@
 import { Image, Images } from '../GameTypes'
 
 const ImagePreloader = () => {
+  const promises = []
+
   const loadImage = (imageName: string) => {
-    return new Promise((resolve, reject) => {
-      const imgElement = document.createElement('img')
-      imgElement.src = `${__dirname}/../images/${imageName}.png`
-      imgElement.onload = () => {
-        resolve({
-          id: imageName,
-          element: imgElement,
-        })
-      }
+    console.log(`Loading image "${imageName}"`)
+    promises.push(
+      new Promise((resolve, reject) => {
+        const imgElement = document.createElement('img')
+        imgElement.src = `http://localhost:8080/images/${imageName}.png`
+        imgElement.onload = () => {
+          resolve({
+            id: imageName,
+            element: imgElement,
+          })
+        }
+        imgElement.onerror = reject
+      })
+    )
+    Promise.all(promises).then((images) => {
+      console.log(images)
+      // resolve(images)
     })
   }
 
-  const preload = async (imageNames: string[]) => {
-    return await imageNames.map(loadImage)
+  const preload = async (images: string[]) => {
+    return await images.map(loadImage)
   }
 
   return { preload }
--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,24 @@ import Player from './modules/Player'
 import './scss/main.scss'
 import imageNames from './../images.json'
 
-// const init = async (): void => {
-//   let images = []
-//   if (imageNames.length) {
-//     images = await ImagePreloader.preload(images)
-//   }
-//   console.log(images)
-//   const things: types.Thing[] = []
-//   const player: types.Thing = Player
-//   const state: types.GameState = {
-//     things,
-//     paused: false,
-//     keysDown: [],
-//   }
+const init = async () => {
+  let images = []
+  if (imageNames.length) {
+    console.log(`Preloading following ${imageNames.length} images:`)
+    console.table(imageNames)
+    images = await ImagePreloader.preload(images)
+    console.log(images)
+  }
 
-//   Runtime.start()
-// }
+  const things: types.Thing[] = []
+  const player: types.Thing = Player
+  const state: types.GameState = {
+    things,
+    paused: false,
+    keysDown: [],
+  }
 
-// init()
+  Runtime.start()
+}
+
+init()
