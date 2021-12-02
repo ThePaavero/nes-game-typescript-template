f94557e89658eb1f5770632b8c470149e2a4b197
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2021-12-02 20:12:31 +0200

    There we go.

--- src/engine/ImagePreloader.ts
+++ src/engine/ImagePreloader.ts
@@ -1,7 +1,7 @@
 import { Image } from '../types/GameTypes'
 
 const ImagePreloader = () => {
-  const loadImage = async (imageName: string) => {
+  const loadImage = async (imageName: string): Promise<Image> => {
     const img = new Image()
     img.src = `/images/${imageName}.png`
     await img.decode()
