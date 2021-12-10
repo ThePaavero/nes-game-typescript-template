14063992935e4a03f6be2696dda7fc0f609a21a5
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2021-12-11 01:51:47 +0200

    Shorter, but harder to read. I hate condensing code for its own sake but feel compelled to do it.

--- src/engine/ImagePreloader.ts
+++ src/engine/ImagePreloader.ts
@@ -1,13 +1,15 @@
 import { Image } from '../types/GameTypes'
 
-const preloadSingleImage = async (imageName: string): Promise<Image> => {
-  const img = document.createElement('img')
-  img.src = `/images/${imageName}.png`
-  await img.decode()
-  return {
-    id: imageName,
-    element: img,
-  }
+export default async (images: string[]): Promise<Image[]> => {
+  return Promise.all(
+    images.map(async (imageName: string): Promise<Image> => {
+      const img = document.createElement('img')
+      img.src = `/images/${imageName}.png`
+      await img.decode()
+      return {
+        id: imageName,
+        element: img,
+      }
+    })
+  )
 }
-
-export default async (images: string[]): Promise<Image[]> => Promise.all(images.map(preloadSingleImage))
