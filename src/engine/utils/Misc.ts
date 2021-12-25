4aef400eb9aef1f8a34b85f0abf1c275a9fa5cc1
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2021-12-26 00:26:40 +0200

    playSound second optional argument: volume.

--- src/engine/utils/Misc.ts
+++ src/engine/utils/Misc.ts
@@ -18,9 +18,9 @@ export const randomIntFromInterval = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-export const playSound = (soundName: string) => {
+export const playSound = (soundName: string, volume?: number): void => {
   const audioElement: HTMLAudioElement = document.querySelector(`.audio-${soundName}`)
-  audioElement.volume = config.audioVolume
+  audioElement.volume = volume || config.audioVolume
   if (!audioElement) {
     console.warn(`Could not load audio element by name "${soundName}"`)
     return
